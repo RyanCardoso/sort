166c53ec9499b20f4708d0449934556237e2d283
ryancardoso <ryanferreiracardoso864@gmail.com>
2022-02-28 12:29:47 -0300

    saving menu position

--- moveMenu.js
+++ moveMenu.js
@@ -1,8 +1,16 @@
 // Variables 
 const menu = document.querySelector('#boxMenu');
 let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+let menuLeft, menuTop;
+
+let savePosX = localStorage.getItem('posXMenu');
+let savePosY = localStorage.getItem('posYMenu');
 
 // Events
+window.addEventListener('load', function () {
+    menu.style.top = savePosY.toString() + "px";
+    menu.style.left = savePosX.toString() + "px"; 
+})
 menu.addEventListener('mousedown', mouseDown);
 
 function mouseDown (e) {
@@ -18,7 +26,7 @@ function mouseDrag (e) {
   pos2 = pos4 - e.clientY;
   pos3 = e.clientX;
   pos4 = e.clientY;
-
+  
   menu.style.top = (menu.offsetTop - pos2) + "px";
   menu.style.left = (menu.offsetLeft - pos1) + "px"; 
 }
@@ -26,4 +34,7 @@ function mouseDrag (e) {
 function mouseUp () {
   document.onmousemove = null;
   menu.style.cursor = 'grab';
+ 
+  localStorage.setItem('posXMenu', menu.offsetLeft);
+  localStorage.setItem('posYMenu', menu.offsetTop);
 }
